5a62b6a0622d8107dc40db6a545c0d4bfce6fd9d
Artur777 <arturarturartur358@gmail.com>
2025-12-29 20:52:45 +0300

    update build script for migrations

--- build.sh
+++ build.sh
@@ -9,5 +9,10 @@ pip install -r requirements.txt
 # Применяем миграции базы данных
 python manage.py migrate --noinput
 
+python manage.py create_superuser_if_none_exists \
+  --username=admin \
+  --email=admin@example.com \
+  --password=123123
+
 # Собираем статические файлы
 python manage.py collectstatic --noinput
